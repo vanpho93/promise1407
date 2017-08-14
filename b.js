4147ef6cc5891b06dcc724f6139a4598eaaa064a
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-14 20:41:04 +0700

    promise funcs

--- b.js
+++ b.js
@@ -1,16 +1,6 @@
 const request = require('request');
 
-function cong(soA, soB, cb) {
-    const isTypeOk = typeof soA !== 'number' || typeof soB !== 'number';
-    if (isTypeOk) return cb(new Error('Type error'));
-    const url = `http://localhost:3000/tinh/CONG/${soA}/${soB}`;
-    request(url, (err, response, data) => {
-        cb(err, data);
-    });
-}
-
-function congPromise(soA, soB) {
-    // Promise
+function cong(soA, soB) {
     return new Promise((resolve, reject) => {
         const isTypeOk = typeof soA !== 'number' || typeof soB !== 'number';
         if (isTypeOk) return reject(new Error('Type error'));
@@ -22,6 +12,27 @@ function congPromise(soA, soB) {
     });
 }
 
-congPromise(5, 10)
-.then(tong => console.log('THANH CONG: ' + tong))
-.catch(err => console.log(err.message));
+function nhan(soA, soB) {
+    return new Promise((resolve, reject) => {
+        const isTypeOk = typeof soA !== 'number' || typeof soB !== 'number';
+        if (isTypeOk) return reject(new Error('Type error'));
+        const url = `http://localhost:3000/tinh/NHAN/${soA}/${soB}`;
+        request(url, (err, response, data) => {
+            if (err) return reject(err);
+            resolve(data);
+        });
+    });
+}
+
+function chia(soA, soB) {
+    return new Promise((resolve, reject) => {
+        const isTypeOk = typeof soA !== 'number' || typeof soB !== 'number';
+        if (isTypeOk) return reject(new Error('Type error'));
+        if (soB === 0) return reject(new Error('Divide by 0'));
+        const url = `http://localhost:3000/tinh/CHIA/${soA}/${soB}`;
+        request(url, (err, response, data) => {
+            if (err) return reject(err);
+            resolve(data);
+        });
+    });
+}
