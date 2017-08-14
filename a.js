f4d28572687bf4078e0db797450c0ec161de0b15
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-14 20:19:11 +0700

    callback lien tiep

--- a.js
+++ a.js
@@ -1,2 +1,46 @@
-// 10, 5, 2 -> 15
-// (dayLon + dayNho) * chieuCao / 2
+const request = require('request');
+
+function cong(soA, soB, cb) {
+    const isTypeOk = typeof soA !== 'number' || typeof soB !== 'number';
+    if (isTypeOk) return cb(new Error('Type error'));
+    const url = `http://localhost:3000/tinh/CONG/${soA}/${soB}`;
+    request(url, (err, response, data) => {
+        cb(err, data);
+    });
+}
+
+function nhan(soA, soB, cb) {
+    const isTypeOk = typeof soA !== 'number' || typeof soB !== 'number';
+    if (isTypeOk) return cb(new Error('Type error'));
+    const url = `http://localhost:3000/tinh/NHAN/${soA}/${soB}`;
+    request(url, (err, response, data) => {
+        cb(err, data);
+    });
+}
+
+function chia(soA, soB, cb) {
+    const isTypeOk = typeof soA !== 'number' || typeof soB !== 'number';
+    if (isTypeOk) return cb(new Error('Type error'));
+    if (soB === 0) return cb(new Error('Divide by 0'));
+    const url = `http://localhost:3000/tinh/CHIA/${soA}/${soB}`;
+    request(url, (err, response, data) => {
+        cb(err, data);
+    });
+}
+
+function tinhDienTicHinhThang(a, b, h, cb) {
+    cong(a, b, (errCong, tong) => {
+        if (errCong) return cb(errCong);
+        nhan(+tong, h, (errNhan, tich) => {
+            if (errNhan) return cb(errNhan);
+            chia(+tich, 2, (errChia, ketQua) => {
+                if (errChia) return cb(errChia);
+                cb(null, ketQua);
+            });
+        });
+    });
+}
+
+tinhDienTicHinhThang(10, 5, 2, (err, square) => {
+    console.log(err, square);
+});
