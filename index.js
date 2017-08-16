ac38f9be93cfebf1031cfc2c04e9e76176f1a35c
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-16 20:47:48 +0700

    ready to push

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const queryDb = require('./db');
+// const queryDb = require('./db');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    queryDb('SELECT * FROM "Product"', [])
-    .then(result => res.render('home', { products: result.rows }))
-    .catch(err => res.send(err.message));
-});
+app.get('/', (req, res) => res.send('Server is running'));
 
-app.listen(3000, () => console.log('Server started!'));
+// app.get('/', (req, res) => {
+//     queryDb('SELECT * FROM "Product"', [])
+//     .then(result => res.render('home', { products: result.rows }))
+//     .catch(err => res.send(err.message));
+// });
+
+app.listen(process.env.PORT || 3000, () => console.log('Server started!'));
